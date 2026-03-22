7e5b087932647a4c61dff5a98ae432b1d5b61c4c
PETER-PHAN\anhdu <minhdat.phan@live.com>
2026-03-22 20:38:40 +0700

    first commit

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template_string
+import os
 
 app = Flask(__name__)
 
@@ -111,4 +112,5 @@ def delete(page_id):
     return "OK"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
